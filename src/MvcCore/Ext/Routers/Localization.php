5408bb225e4d89010856b67e93056cbc09129745
Tom Flídr <tomflidr@gmail.com>
2018-12-18 12:05:49 +0100

    VERSION constant moved from interfaces into base classes where necessary

--- src/MvcCore/Ext/Routers/Localization.php
+++ src/MvcCore/Ext/Routers/Localization.php
@@ -27,6 +27,13 @@ extends		\MvcCore\Router
 implements	\MvcCore\Ext\Routers\ILocalization,
 			\MvcCore\Ext\Routers\IExtended
 {
+	/**
+	 * MvcCore Extension - Router Media - version:
+	 * Comparison by PHP function version_compare();
+	 * @see http://php.net/manual/en/function.version-compare.php
+	 */
+	const VERSION = '5.0.0-alpha';
+
 	use \MvcCore\Ext\Routers\Extended;
 
 	use \MvcCore\Ext\Routers\Localization\Preparing;
@@ -39,11 +46,4 @@ implements	\MvcCore\Ext\Routers\ILocalization,
 	use \MvcCore\Ext\Routers\Localization\UrlByRoute;
 	use \MvcCore\Ext\Routers\Localization\UrlByRouteSections;
 	use \MvcCore\Ext\Routers\Localization\UrlByRouteSectionsLocalization;
-	
-	/**
-	 * MvcCore Extension - Router Lang - version:
-	 * Comparison by PHP function version_compare();
-	 * @see http://php.net/manual/en/function.version-compare.php
-	 */
-	const VERSION = '5.0.0-alpha';
 }
